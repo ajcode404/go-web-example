cb77bc8382c4e9abf3b002611c01a9c07a5eb72d
Akash Jain <ajcodes.tech@gmail.com>
2024-10-29 19:18:25 +0530

    DB mysql

--- mysql-db/main.go
+++ mysql-db/main.go
@@ -12,19 +12,21 @@ import (
 func main() {
 
 	// don't put username and password in the code, this is just an example code.
-	db, err := sql.Open("mysql", "root:password@(localhost:3306)/test?parseTime=true")
+	db, err := sql.Open("mysql", "akash:password@(localhost:3306)/test?parseTime=true")
 	if err != nil {
 		log.Fatal(err)
 	}
-	v, err := db.Exec("SELECT 1")
+	createUsers(db)
+	insertUser(db)
+	printUsers(db)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print(v)
-	fmt.Println("accessed")
 }
 
 func createUsers(db *sql.DB) {
+	dropTableIfExist(db)
 	query := `
 	CREATE TABLE users (
 		id INT AUTO_INCREMENT,
@@ -33,7 +35,15 @@ func createUsers(db *sql.DB) {
 		created_at DATETIME,
 		PRIMARY KEY (id)
 	);`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
 
+func dropTableIfExist(db *sql.DB) {
+	query := `DROP TABLE IF EXISTS users;`
 	if _, err := db.Exec(query); err != nil {
 		log.Fatal(err)
 		return
@@ -52,12 +62,44 @@ func insertUser(db *sql.DB) {
 		return
 	}
 
-	id, err := result.LastInsertId()
+	if _, err := result.LastInsertId(); err != nil {
+		log.Fatal(err)
+		return
+	}
+}
 
+func printUsers(db *sql.DB) {
+	type user struct {
+		id         int
+		username   string
+		password   string
+		created_at time.Time
+	}
+
+	query := `select id, username, password, created_at from users;`
+
+	rows, err := db.Query(query)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+	defer rows.Close()
+	var users []user
+	for rows.Next() {
+		var u user
+		err := rows.Scan(&u.id, &u.username, &u.password, &u.created_at)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		users = append(users, u)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+		return
+	}
+	for _, u := range users {
+		fmt.Printf("id = %d\nusername = %s\npassword=%s\ntime=%s", u.id, u.username, u.password, u.created_at)
+	}
 
-	fmt.Println(id)
 }
